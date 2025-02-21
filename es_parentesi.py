c9d73aa1c3909a797399bd17d105c7ea28484bef
safe-wind <gdl.marius0@gmail.com>
2025-02-21 18:25:34 +0100

    esercizio svolto

--- es_parentesi.py
+++ es_parentesi.py
@@ -2,31 +2,31 @@
 
 domanda:str = "Inserisci una serie di parentesi e verifica se sono bilanciate: "
 print(domanda)
-bilanciate:str = input(str())
+parentesi:str = input(str())
 
-open_par = 0
-close_par = 0
+count_par = 0
 output = True
 
-for par in bilanciate:
+for par in parentesi:
 
     if par=="(":
-        open_par += 1
+
+        count_par += 1
+
     elif par==")":
-        close_par += 1
-    else:
-        output = False
+
+        count_par -= 1
+
+    if count_par < 0 or count_par>0:
+        output=False
         break
 
-    if close_par <= open_par:
-        output= True
-    elif close_par > open_par:
-        output= False
-        break
+print(output)
+    
+
+
+    
     
         
-if open_par > close_par:
-    print(f"Sono bilanciate?: {output}")
-else:
-    print(f"Sono bilanciate?: {output}")
 
+        
