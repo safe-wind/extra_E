d95dafc177e0bb82bb9531c27882f7cf73e466d0
safe-wind <gdl.marius0@gmail.com>
2025-02-22 17:08:12 +0100

    esercizio extra

--- es_parentesi.py
+++ es_parentesi.py
@@ -1,32 +1,51 @@
+
 #es_parentesi
+'''
+caso1:str = "((()))"
+caso2:str = "(()"
+caso3:str = "()()()"
+caso4:str = "))(("
+caso5:str = "(()())"
+'''
+print("Scrivi una serie di parentesi:")
 
-domanda:str = "Inserisci una serie di parentesi e verifica se sono bilanciate: "
-print(domanda)
-parentesi:str = input(str())
+caso = input(str(""))
 
-count_par = 0
-output = True
+#inizializzo la variabile output a false in caso la stringa sia vuota
+# e inizio 2 contatori di caratteri nella stringa
 
-for par in parentesi:
+par_a:int = 0
+par_c:int = 0
+output:bool = False
+
+# Itero nella stringa e incremento 
+# un contatore ogni volta che si incontra 
+# una par_a o par_c
+
+for par in caso:
 
     if par=="(":
-
-        count_par += 1
+        par_a+=1
 
     elif par==")":
+        par_c+=1
 
-        count_par -= 1
+# se i caratteri sono diversi da  "(" o  ")",
+# finisce il ciclo
 
-    if count_par < 0 or count_par>0:
-        output=False
+    else:
+        output = False
         break
 
+# sempre nel ciclo verifico se ci sono sbilanciamenti tra le par_a e le par_c
+# se sono uguali sono bilanciate e output è True 
+# ma se le par_a vengono dopo le par_c nell'iterazione allora l'output è False e interrompo il ciclo
+
+    if par_c == par_a:
+        output=True
+    elif par_a < par_c:
+        output=False
+        break
+        
+
 print(output)
-    
-
-
-    
-    
-        
-
-        
